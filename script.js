6fca13d334849465af7ead8b29d81855c31d1d3b
PaulMazur <warriorofcode@gmail.com>
2022-05-12 21:36:19 +0200

    write promise version dns.lookup method

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const { lookup } = require('dns');
+const { promisify } = require('util');
 
-lookup('google.com', (err, address) => {
-  if (err) { console.log(err.message); } else { console.log(address); }
-});
+const hostPromise = promisify(lookup);
+
+hostPromise('google.com')
+  .then((address) => console.log(address))
+  .catch((err) => console.log(err));
